aefbf4d1ad1f37f0c3ca72f6f3d96c4c47c2de7e
AbuBakar <muhammadabubakar19187@gmail.com>
2025-08-29 08:43:16 +0500

    Sidebar responsiveness completed

--- components/SocialMedia.tsx
+++ components/SocialMedia.tsx
@@ -1,6 +1,7 @@
 import { Facebook, Github, Linkedin, Slack, Youtube } from 'lucide-react'
 import React from 'react'
-import { Tooltip, TooltipProvider, TooltipTrigger } from './ui/tooltip'
+import { Tooltip, TooltipProvider, TooltipTrigger, TooltipContent  } from './ui/tooltip'
+import { cn } from '@/lib/utils'
 import Link from 'next/link'
 
 const socialLink = [
@@ -40,14 +41,15 @@ interface Props {
 const SocialMedia = ({className, iconClassName, tooltipClassName}:Props) => {
   return (
     <TooltipProvider>
-      <div>
+      <div className={cn("flex items-center gap-3.5", className)}>
         {socialLink?.map((item) => (
           <Tooltip>
-            <TooltipTrigger>
-              <Link key={item?.title} href={item?.href}>
+            <TooltipTrigger asChild>
+              <Link key={item?.title} target='_blank' rel='noopener noreferrer' href={item?.href} className={cn("p-2 border rounded-full hover:text-white hover:border-shop_light_green hoverEffect", iconClassName)}>
                 {item?.icon}
               </Link>
             </TooltipTrigger>
+            <TooltipContent className={cn("bg-white text-darkColor font-semibold", tooltipClassName)}>{item?.title}</TooltipContent>
           </Tooltip>
         ))}
       </div>
--- where_to_continue.md
+++ where_to_continue.md
@@ -1,8 +1,16 @@
+<!-- DAY 1 -->
+
 <!-- 39:00
 npx shadcn@latest add "components goes here" -->
 
 <!-- 1:06:58
 Making the navbar middle part -->
 
-1:42:27
-Making responsive header 
+
+
+<!-- DAY 2 -->
+
+<!-- 1:46:42
+Making responsive header - almost done -->
+
+
