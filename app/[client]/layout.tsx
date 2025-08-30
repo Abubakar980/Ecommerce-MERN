47034a05114d422cdecd4dd1a39366944e8814c9
AbuBakar <muhammadabubakar19187@gmail.com>
2025-08-30 23:07:03 +0500

    change some files locations for sanity

---
+++ app/[client]/layout.tsx
@@ -0,0 +1,33 @@
+import type { Metadata } from "next";
+import "../globals.css";
+import Footer from "@/components/Footer";
+import Header from "@/components/Header";
+import {ClerkProvider} from "@clerk/nextjs";
+
+
+
+export const metadata: Metadata = {
+  title: {
+    template: "%s - ShopCart online Store",
+    default: "ShopCart online store"
+  },
+  description: "ShopCart online store, your one-stop shop for all things fashion and lifestyle.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <ClerkProvider>
+        <div className="flex flex-col min-h-screen">
+          <Header/>
+            <main className="flex-1">
+              {children}
+            </main>
+          <Footer/>
+        </div>
+    </ClerkProvider>
+  );
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,37 +1,11 @@
-import type { Metadata } from "next";
-import "./globals.css";
-import Footer from "@/components/Footer";
-import Header from "@/components/Header";
-import {ClerkProvider} from "@clerk/nextjs";
-
-
-
-export const metadata: Metadata = {
-  title: {
-    template: "%s - ShopCart online Store",
-    default: "ShopCart online store"
-  },
-  description: "ShopCart online store, your one-stop shop for all things fashion and lifestyle.",
-};
-
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
-  return (
-    <ClerkProvider>
-    <html lang="en">
-      <body className="font-poppins antialiased">
-        <div className="flex flex-col min-h-screen">
-          <Header/>
-            <main>
-              {children}
-            </main>
-          <Footer/>
-        </div>
-      </body>
-    </html>
-    </ClerkProvider>
-  );
+const RootLayout = ({children}:{children:React.ReactNode}) => {
+    return (
+        <html lang="en">
+            <body className="font-poppins antialiased">
+                {children}
+            </body>
+        </html>
+    )
 }
+
+export default RootLayout
